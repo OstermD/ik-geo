9436395052221ffc62bbbfebb0834cc62b4d09a1
Daniel Ostermeier <ostermeier_Daniel@t-online.de>
2024-06-04 15:16:43 +0200

    Additional error check for SP3 - checks for all SPs pending

--- cpp/subproblems/sp.cpp
+++ cpp/subproblems/sp.cpp
@@ -401,7 +401,7 @@ namespace IKS
 	{
 		const Eigen::Vector3d kxp = k.cross(p1);
 		Eigen::Matrix<double, 3, 2> a_1;
-		if(kxp.isZero(ZERO_THRESH))
+		if(kxp.isZero(ZERO_THRESH) || k.cross(p2).isZero(ZERO_THRESH))
 		{
 			// Minimization is independent of theta -> probably redundant!
 			//std::cout<<"Warning! - minimization of subproblem 3 is indipendent of theta. This may indicate redundancy"<<std::endl;
